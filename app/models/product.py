b3e4e171723db083cbc737ac50298b0238e78d95
Erfan Rezaei <erfan.rezaei@aasood.com>
2022-05-15 15:56:54 +0430

    getting visiblity and image and label from kowsar

--- app/models/product.py
+++ app/models/product.py
@@ -14,67 +14,77 @@ class Product(ABC):
     @staticmethod
     def get_all_categories():
         with MongoConnection() as mongo:
-            with RedisConnection() as redis_db:
-                result = mongo.collection.find({}, {"_id": 0, "main_category": 1, "sub_category": 1, "system_code": 1,
-                                                    "brand": 1})
+            result = mongo.collection.find({}, {"_id": 0, "main_category": 1, "sub_category": 1, "system_code": 1,
+                                                "brand": 1})
 
-                tree_data = []
-                for obj in result:
-                    stored_main = next((x for x in tree_data if x['name'] == obj.get("main_category")), None)
-                    if stored_main:
-                        index_main = tree_data.index(stored_main)
-                        stored_sub = next(
-                            (x for x in tree_data[index_main]['children'] if x['name'] == obj.get("sub_category"))
-                            , None)
-                        if stored_sub:
-                            index_sub = tree_data[index_main]['children'].index(stored_sub)
-                            if next((x for x in tree_data[index_main]['children'][index_sub]['children'] if
-                                     x['name'] == obj.get("brand")), None):
-                                continue
-                            tree_data[index_main]['children'][index_sub]['children'].append(
-                                {
-                                    'name': obj.get("brand"),
-                                    'title': redis_db.client.hget(obj.get("brand"), "fa_ir"),
-                                    "system_code": obj.get("system_code")[:6]
-                                }
-                            )
+            tree_data = []
+            for obj in result:
+                kowsar_data = list(mongo.kowsar_collection.find({"system_code": {"$in": [obj.get("system_code")[:2],
+                                                                                         obj.get("system_code")[:4],
+                                                                                         obj.get("system_code")[:6],
+                                                                                         ]}}, {"_id": 0}).sort(
+                    "system_code", 1))
+                stored_main = next((x for x in tree_data if x['name'] == obj.get("main_category")), None)
+                if stored_main:
+                    index_main = tree_data.index(stored_main)
+                    stored_sub = next(
+                        (x for x in tree_data[index_main]['children'] if x['name'] == obj.get("sub_category"))
+                        , None)
+                    if stored_sub:
+                        index_sub = tree_data[index_main]['children'].index(stored_sub)
+                        if next((x for x in tree_data[index_main]['children'][index_sub]['children'] if
+                                 x['name'] == obj.get("brand")), None):
                             continue
-                        tree_data[index_main]['children'].append(
+                        tree_data[index_main]['children'][index_sub]['children'].append(
                             {
-                                'name': obj.get("sub_category"),
-                                'title': redis_db.client.hget(obj.get("sub_category"), "fa_ir"),
-                                "system_code": obj.get("system_code")[:4],
-                                'children': [
-                                    {
-                                        'name': obj.get("brand"),
-                                        'title': redis_db.client.hget(obj.get("brand"), "fa_ir"),
-                                        "system_code": obj.get("system_code")[:6]
-                                    }
-                                ]
+                                'name': obj.get("brand"),
+                                'title': kowsar_data[2].get("brand_label", obj.get("brand")),
+                                'image': kowsar_data[2].get("image", None),
+                                "system_code": obj.get("system_code")[:6]
                             }
                         )
                         continue
-                    tree_data.append({
-                        'name': obj.get("main_category"),
-                        'title': redis_db.client.hget(obj.get("main_category"), "fa_ir"),
-                        "system_code": obj.get("system_code")[:2],
-                        "children": [
-                            {
-                                'name': obj.get("sub_category"),
-                                'title': redis_db.client.hget(obj.get("sub_category"), "fa_ir"),
-                                "system_code": obj.get("system_code")[:4],
-                                "children": [
-                                    {
-                                        'name': obj.get("brand"),
-                                        'title': redis_db.client.hget(obj.get("brand"), "fa_ir"),
-                                        "system_code": obj.get("system_code")[:6]
-                                    }
-                                ]
-                            }
-                        ]
-                    })
+                    tree_data[index_main]['children'].append(
+                        {
+                            'name': obj.get("sub_category"),
+                            'title': kowsar_data[1].get("sub_category_label", obj.get("sub_category")),
+                            'image': kowsar_data[1].get("image", None),
+                            "system_code": obj.get("system_code")[:4],
+                            'children': [
+                                {
+                                    'name': obj.get("brand"),
+                                    'title': kowsar_data[2].get("brand_label", obj.get("brand")),
+                                    'image': kowsar_data[2].get("image", None),
+                                    "system_code": obj.get("system_code")[:6]
+                                }
+                            ]
+                        }
+                    )
+                    continue
+                tree_data.append({
+                    'name': obj.get("main_category"),
+                    'title': kowsar_data[0].get("main_category_label", obj.get("main_category")),
+                    'image': kowsar_data[0].get("image", None),
+                    "system_code": obj.get("system_code")[:2],
+                    "children": [
+                        {
+                            'name': obj.get("sub_category"),
+                            'title': kowsar_data[1].get("sub_category_label", obj.get("sub_category")),
+                            'image': kowsar_data[1].get("image", None),
+                            "system_code": obj.get("system_code")[:4],
+                            "children": [
+                                {
+                                    'name': obj.get("brand"),
+                                    'title': kowsar_data[2].get("brand_label", obj.get("brand")),
+                                    'image': kowsar_data[2].get("image", None),
+                                    "system_code": obj.get("system_code")[:6]
+                                }
+                            ]
+                        }
+                    ]
+                })
 
-                return tree_data
+            return tree_data
 
     @staticmethod
     def get(system_code: str = None, page: int = 1, per_page: int = 10):
