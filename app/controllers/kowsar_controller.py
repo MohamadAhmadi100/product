f3f270fba44de43e870cb8b6c3cc37e17b1b73f3
Erfan Rezaei <erfan.rezaei@aasood.com>
2022-06-14 16:09:46 +0430

    added new apis

--- app/controllers/kowsar_controller.py
+++ app/controllers/kowsar_controller.py
@@ -1,3 +1,4 @@
+from app.models.kowsar import KowsarPart, KowsarGroup
 from app.modules.kowsar_getter import KowsarGetter
 from app.modules.translator import update_redis_database
 
@@ -33,3 +34,45 @@ def update_kowsar_collection():
     KowsarGetter.create_new_parents()
     update_redis_database()
     return {"success": True, "message": "با موفقیت به روز رسانی شد", "status_code": 200}
+
+
+def create_system_code(model_code, config, storage_ids):
+    """
+    create system code from model code
+    """
+    kowsar = KowsarPart(model_code, config)
+    data = KowsarGetter.system_code_name_getter(model_code)
+    if not data:
+        return {"success": False, "status_code": 404, "error": "کالای مورد نظر یافت نشد"}
+    name = kowsar.name_getter()
+    name = data.get("brand", "").upper() + " " + data.get("model", "").upper() + " " + name.upper()
+    system_code = kowsar.system_code_getter(model_code)
+    result, system_code = kowsar.create_kowsar_part(name, storage_ids, model_code + system_code)
+    if result:
+        kowsar.create_in_db(data, system_code)
+        return {"success": True, "message": {
+            "system_code": system_code,
+            "message": "با موفقیت ایجاد شد"
+        }, "status_code": 200}
+    return {"success": False, "error": "ساخت محصول با خطا مواجه شد", "status_code": 500}
+
+
+def create_kowsar_group(system_code, name, parent_system_code):
+    """
+    create kowsar group
+    """
+    kowsar = KowsarGroup(system_code, name, parent_system_code)
+    if not kowsar.is_unique():
+        return {"success": False, "error": "گروه مورد نظر قبلا ساخته شده است", "status_code": 400}
+    parent_data = KowsarGetter.system_code_name_getter(parent_system_code)
+    result = kowsar.create_kowsar_group(parent_data)
+    if result:
+        complete_data = kowsar.category_name_getter(parent_data)
+        mongo_result = kowsar.create_in_db(complete_data)
+        if not mongo_result:
+            return {"success": False, "error": "ساخت گروه در دیتابیس با خطا مواجه شد", "status_code": 500}
+        return {"success": True, "message": {
+            "system_code": system_code,
+            "message": "با موفقیت ایجاد شد"
+        }, "status_code": 200}
+    return {"success": False, "error": "ساخت گروه با خطا مواجه شد", "status_code": 500}
---
+++ app/models/kowsar.py
@@ -0,0 +1,146 @@
+import requests
+
+from app.helpers.mongo_connection import MongoConnection
+
+
+class KowsarPart:
+    def __init__(self, model_code, config):
+        self.model_code = model_code
+        config = {k: v for k, v in config.items() if v}
+        self.config = config
+
+    def create_kowsar_part(self, name, storage_ids, system_code):
+        request_data = {
+            "PartPartStoreDTOLst": [{"inv_Store_Code": str(i)} for i in storage_ids],
+            "PartGroupCodeLst": [
+                f"{self.model_code}"
+            ],
+            "prt_Part_Code": system_code,
+            "prt_PartGroup_CodeMain": f"{self.model_code[:4]}",
+            "prt_Part_Name": name,
+            "prt_Part_HasQTYControl": True,
+            "prt_PartUnit_Code": "2",
+            "inv_PartClass_Code": f"{self.model_code[:2]}",
+            "sel_PartCategory_Code": f"{self.model_code[:2]}",
+            "gnr_Lookup_InternalIdDecIncrementGroup": "1",
+            "gnr_Lookup_InternalIdSerialState": "2",
+            "prt_Part_Sellable": "0"  # be 1 on production
+        }
+        # result = requests.post("http://31.47.52.130:8099/PartService/Web/TryInsertPart2", json=request_data, headers={
+        #     "UserName": "Site",
+        #     "Password": "Site@3333"
+        # }).json()
+        result = {
+            "HasError": False,
+            "prt_Part_Code": system_code,
+        }
+        if not result.get("HasError", True):
+            return True, result.get("prt_Part_Code")
+        return False, None
+
+    def name_getter(self):
+        name = "["
+        config_len = self.config.__len__() - 1
+        count = 0
+        for config, value in self.config.items():
+            if count == config_len:
+                name += f"{value}]"
+            else:
+                name += f"{value}-"
+            count += 1
+        return name
+
+    def system_code_getter(self, model_code):
+        with MongoConnection() as mongo:
+            regex = f"^{model_code}."
+            regex += "{3}"
+            system_codes = mongo.kowsar_collection.distinct("system_code",
+                                                            {"system_code": {"$regex": regex}})
+            if system_codes:
+                system_codes = [int(i[-3:]) for i in system_codes]
+                system_codes.sort()
+                system_code = system_codes[-1] + 1
+                if system_code < 10:
+                    system_code = f"00{system_code}"
+                elif system_code < 100:
+                    system_code = f"0{system_code}"
+                else:
+                    system_code = f"{system_code}"
+
+            return system_code
+
+    def create_in_db(self, model_data, system_code):
+        with MongoConnection() as mongo:
+            del model_data["system_code"]
+            result = mongo.kowsar_collection.insert_one(
+                {
+                    "system_code": system_code,
+                    **model_data,
+                    "config": self.config
+                }
+            )
+        if result.inserted_id:
+            return True
+        return False
+
+
+class KowsarGroup:
+    def __init__(self, system_code, name, parent_system_code):
+        self.system_code = system_code
+        self.name = name
+        self.parent_system_code = parent_system_code
+
+    def is_unique(self):
+        with MongoConnection() as mongo:
+            result = mongo.kowsar_collection.find_one({"system_code": self.system_code})
+            if result:
+                return False
+            return True
+
+    def create_kowsar_group(self, parent_data):
+        request_data = {
+            "prt_PartGroup_Code": self.system_code,
+            "prt_PartGroup_CodeParent": self.parent_system_code,
+            "prt_PartGroup_Name": self.name,
+        }
+        if len(self.system_code) == 9:
+            request_data['SaveAsFormalAcc'] = True
+            accformal_name = parent_data.get("sub_category") if parent_data.get(
+                "sub_category") != "Mobile" else "mobile"
+            request_data['acc_FormalGrouping_NameMain'] = accformal_name
+
+        # result = requests.post("http://31.47.52.130:8099/PartService/Web/TryInsertPartGroup", json=request_data,
+        #                        headers={
+        #                            "UserName": "Site",
+        #                            "Password": "Site@3333"
+        #                        }).json()
+        result = {
+            "HasError": True
+        }
+        if not result.get("HasError", True):
+            return True
+        return False
+
+    def category_name_getter(self, parent_data):
+        if parent_data.get("image"):
+            del parent_data["image"]
+        if parent_data.get('visible_in_site'):
+            del parent_data["visible_in_site"]
+
+        system_code_len = len(parent_data.get("system_code"))
+        if system_code_len == 2:
+            parent_data['sub_category'] = self.name
+        elif system_code_len == 4:
+            parent_data['brand'] = self.name
+        elif system_code_len == 6:
+            parent_data['model'] = self.name
+
+        parent_data['system_code'] = self.system_code
+        return parent_data
+
+    def create_in_db(self, data):
+        with MongoConnection() as mongo:
+            result = mongo.kowsar_collection.insert_one(data)
+        if result.inserted_id:
+            return True
+        return False
