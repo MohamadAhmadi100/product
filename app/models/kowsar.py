0828d46f3af9a0fbd4886ab9402672658f45e116
Erfan Rezaei <erfan.rezaei@aasood.com>
2022-06-15 14:24:07 +0430

    fix category group

--- app/models/kowsar.py
+++ app/models/kowsar.py
@@ -103,11 +103,35 @@ class KowsarGroup:
             "prt_PartGroup_CodeParent": self.parent_system_code,
             "prt_PartGroup_Name": self.name,
         }
+        codes = {
+            "mobile": "200000", "Tablet": "200001", "Notebook": "200002",
+            "All in One": "200003", "PC": "200004", "Server": "200005",
+            "Game Console": "200006", "Camera": "200007", "CPU": "200008",
+            "Motherboard": "200009", "Graphic Card": "200010", "RAM": "200011",
+            "Internal HDD": "200012", "Internal DVD": "200013", "Internal SSD": "200014",
+            "LAN Card": "200015", "Monitor": "200016", "Case": "200017",
+            "Power Supply": "200018", "CPU FAN": "200019", "NB Battery": "200020",
+            "External HDD": "200021", "External SSD": "200022", "Flash Memory": "200023",
+            "Memory Card/Stick": "200024", "PowerBank": "200025", "Speaker": "200026",
+            "Charger": "200027", "Mouse": "200028", "Keyboard": "200029",
+            "Keyboard & Mouse": "200030", "Headphone/Headset/Hands Free": "200031",
+            "BackPack/HandyBag": "200032", "Cover/Case": "200033", "Screen Protector": "200034",
+            "Stand/Holder": "200035", "USB HUB": "200036", "Cable": "200037",
+            "Converter": "200038", "WebCam": "200039", "Mouse Pad": "200040",
+            "Band": "200041", "Watch": "200042", "pen": "200043", "Game/Software": "200044",
+            "Battery": "200045", "Cooling": "200046", "Power Port": "200047",
+            "TV": "200048", "External DVD": "200049", "Gajet": "200050",
+            "Modem": "200051", "Access Point": "200052", "Switch/Router": "200053",
+            "Printer": "200054", "Scanner": "200055", "Copy-Machine": "200056",
+            "Data Video Projector": "200057", "Telephone": "200058", "Fax": "200059",
+            "Barcode Reader": "200060", "Cartridge": "200061", "Gaming": "200062", "Vacuum Cleaner": "200063"
+        }
         if len(self.system_code) == 9:
             request_data['SaveAsFormalAcc'] = True
             accformal_name = parent_data.get("sub_category") if parent_data.get(
                 "sub_category") != "Mobile" else "mobile"
-            request_data['acc_FormalGrouping_NameMain'] = accformal_name
+            request_data['acc_FormalGrouping_NameMain'] = "گروهای کالا"
+            request_data['acc_FormalAcc_Code'] = codes.get(accformal_name)
 
         # result = requests.post("http://31.47.52.130:8099/PartService/Web/TryInsertPartGroup", json=request_data,
         #                        headers={
