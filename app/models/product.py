ba16dd3199d139b3da0f280794e861be0cab6630
Erfan Rezaei <erfan.rezaei@aasood.com>
2022-11-22 15:26:12 +0330

    fix ? pricelist

--- app/models/product.py
+++ app/models/product.py
@@ -365,7 +365,7 @@ class Product:
                                                         {
                                                             '$in': [
                                                                 '$$this.v', [
-                                                                    None, 'RX'
+                                                                    None, 'RX', "?"
                                                                 ]
                                                             ]
                                                         }, '', {
