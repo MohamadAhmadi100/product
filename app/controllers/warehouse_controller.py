724e7e00021d4704972bbbd8b4eb432584c40310
Erfan Rezaei <erfan.rezaei@aasood.com>
2023-01-17 17:07:09 +0330

    added create warehouse

--- app/controllers/warehouse_controller.py
+++ app/controllers/warehouse_controller.py
@@ -8,3 +8,11 @@ def all_warehouses():
 def warehouse(storage_id):
     return find_warehouse(storage_id)
 
+
+def create_warehouse(user_id, user_name, warehouse_id, warehouse_name, state, state_id, city, city_id, address):
+    result = create_warehouse_db(
+        user_id, user_name, warehouse_id, warehouse_name, state, state_id, city, city_id, address
+    )
+    if result:
+        return {"success": True, "message": "warehouse created successfully", "status_code": 201}
+    return {"success": False, "error": "something went wrong", "status_code": 404}
--- app/helpers/warehouses.py
+++ app/helpers/warehouses.py
@@ -1,3 +1,5 @@
+import jdatetime
+
 from app.helpers.mongo_connection import MongoConnection
 
 
@@ -14,3 +16,38 @@ def warehouses():
     with MongoConnection() as client:
         warehouses = list(client.warehouses.find({}, {"_id": False}))
         return {"success": True, "warehouses": warehouses}
+
+
+def create_warehouse_db(user_id, user_name, warehouse_id, warehouse_name, state, state_id, city, city_id, address):
+    with MongoConnection() as mongo:
+        result = mongo.warehouses.insert_one({
+            "user_id": user_id,
+            "user_name": user_name,
+            "warehouse_id": warehouse_id,
+            "warehouse_name": warehouse_name,
+            "kosar_id": warehouse_id,
+            "warehouse_status": 1,
+            "warehouse_type": [
+                "virtual",
+                "physical"
+            ],
+            "establishment_status": "static",
+            "assigned_customers": None,
+            "assigned_cities": None,
+            "state": state,
+            "state_id": state_id,
+            "city": city,
+            "city_id": city_id,
+            "address": address,
+            "warehouse_tel": "",
+            "warehouse_admin": "",
+            "warehouse_admin_tel": "",
+            "logistic_admin": "",
+            "logistic_admin_tel": "",
+            "staff": [],
+            "insert_date": jdatetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "isActive": False
+        })
+    if result.inserted_id:
+        return True
+    return False
